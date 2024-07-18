b0c024e8413f47a27f47ed5c39504677caf7fc7e
Nicholas Londey <londey@gmail.com>
2024-07-18 20:47:59 +1000

    work in progress

---
+++ app/src/main.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use panic_halt as _; // panic handler
+
+#[entry]
+fn main() -> ! {
+	// Initialization code here
+
+	loop {
+		// Main loop code here
+	}
+}
